b6c4a0665f167906e3ddf2ac1f125e20366121dc
Ilham Riky Rismawan <166375320+dipdown@users.noreply.github.com>
2024-06-10 20:55:21 +0700

    Update running.sh

--- Nubit/running.sh
+++ Nubit/running.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Install dos2unix
-sudo apt-get update
-sudo apt-get install -y dos2unix
+apt install dos2unix
 
 # Download the light-nodes.sh script
 wget https://raw.githubusercontent.com/dipdown/Node-Crypto/main/light-nodes.sh -O light-nodes.sh
